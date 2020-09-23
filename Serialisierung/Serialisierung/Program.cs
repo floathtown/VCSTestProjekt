ea111ab1a7a58fd87a1e41c504207280638cc25e
floathtown <71782675+floathtown@users.noreply.github.com>
2020-09-23 18:55:32 +0200

    csproj

---
+++ Serialisierung/Serialisierung/Program.cs
@@ -0,0 +1,48 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Serialisierung
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //XDocument doc = XDocument.Parse("<root><A>Irgendein Text</A><B name=\"BBB\"/></root>"); //or XDocument.Load(path)
+            //string jsonText = JsonConvert.SerializeXNode(doc);
+            //dynamic dyn = JsonConvert.DeserializeObject<ExpandoObject>(jsonText);
+            //var text = dyn.root.B("@name");
+            MetaDataItem[] metaDataItems = new MetaDataItem[2];
+            //metaDataItems[0] = new MetaDataItem();
+            //metaDataItems[0].Name = "Fall.ZustaendigerSachbearbeiter.Vorname3";
+            //metaDataItems[0].Value = "Mustermann";
+
+            //metaDataItems[1] = new MetaDataItem();
+            //metaDataItems[1].Name = "Dokument.Druck.Sachbearbeiter.Vorname3";
+            //metaDataItems[1].Value = "Mustermann2";
+
+            var firstnameItemNames = new List<string>() { "Vorgang.Sachbearbeiter.Vorname", "Fall.ZustaendigerSachbearbeiter.Vorname", "Dokument.Druck.Sachbearbeiter.Vorname" };
+            var value = FindItemValue(metaDataItems,firstnameItemNames.Find(f => FindItemValue(metaDataItems, f) != ""));
+            //string name = Array.Find(metaDataItems, f => f.Name.Equals("Name"))?.Value ?? "";
+            //Console.WriteLine(name);
+
+        }
+
+        private static string FindItemValue(MetaDataItem[] metaDataItems, string name)
+        {
+            if (string.IsNullOrWhiteSpace(name)) { return ""; }
+            return Array.Find(metaDataItems, f => f.Name.Equals(name))?.Value ?? "";
+        }
+    }
+
+    public class MetaDataItem
+    {
+        public string Name;
+        public string Value;
+    }
+}
